09a23ba04550e52483147e5379eef714392b721a
vbreault <vbreault@email.com>
2019-11-01 23:17:50 -0400

    progres méthode 1 et 2

--- Sources fournies/aRemettrePourTP2.cpp
+++ Sources fournies/aRemettrePourTP2.cpp
@@ -12,15 +12,57 @@ using namespace std;
 //! \throws logic_error si une incohérence est détecté lors de cette étape de construction du graphe
 void ReseauGTFS::ajouterArcsVoyages(const DonneesGTFS & p_gtfs)
 {
+
+    // m_arret du sommet à la position k = i*j +j
+    unsigned int i = 0;
+    unsigned int j = 0;
+    for (auto voy : p_gtfs.getVoyages()){
+        for (auto arr : voy.second.getArrets()){
+
+            unsigned int temps = arr->getHeureArrivee() - arr->getHeureDepart();
+            m_leGraphe.ajouterArc(i, j, temps);
+            m_arretDuSommet.push_back(arr);
+            m_sommetDeArret.insert({arr, i*j +j});
+            j++;
+        }
+        j = 0;
+        i++;
+    }
 	//écrire votre code ici
 }
 
 
-//! \brief ajouts des arcs dus aux transferts entre stations
+
+//! /! \brief ajouts des arcs dus aux transferts entre stations
 //! \throws logic_error si une incohérence est détecté lors de cette étape de construction du graphe
 void ReseauGTFS::ajouterArcsTransferts(const DonneesGTFS & p_gtfs)
 {
-	//écrire votre code ici
+
+    //comme le nom dit lui cest les transferts et les stations
+    //cette fonction là execute 492 fois
+
+    std::map<unsigned int, Station> toutes_stations = p_gtfs.getStations();
+
+    unsigned int counter = 0;
+    for (auto trans : p_gtfs.getTransferts()){
+
+        unsigned int from = get<0>(trans);
+        unsigned int to = get<1>(trans);
+        unsigned int transfer_time =  get<1>(trans);
+        Station from_stat = toutes_stations[from];
+        Station to_stat = toutes_stations[to];
+        for (auto from_heure_et_arret : from_stat.getArrets()){
+            Arret::Ptr from_arret = from_heure_et_arret.second;
+            cout << "yen a tu vrm un?" << endl;
+            cout << m_sommetDeArret[from_arret] << endl;
+            for (auto to_heure_et_arret: to_stat.getArrets()){
+                Arret::Ptr to_arret = from_heure_et_arret.second;
+                
+        }
+    }
+
+    }
+
 }
 
 //! \brief ajouts des arcs d'une station à elle-même pour les stations qui ne sont pas dans DonneesGTFS::m_stationsDeTransfert
